274a5f3b2ac553dfc1702a9ae1a20fa796c94588
BozzleMay <77677841+BozzleMay@users.noreply.github.com>
2021-11-24 19:29:51 +0000

    Add files via upload

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -3,9 +3,6 @@ import './Cube.css'
 
 import ButtonControls from './ButtonControls';
 
-// const styles = {
-//     cubeStyle:{ animation: 'turn 20s linear infinite'}
-// }
 
 const Cube = () => {
     //set initial state of each cube face
@@ -28,7 +25,7 @@ const Cube = () => {
     }
 }
 
-
+    // Switch statement to handle the cube movements, each case a different movement
     const handleClickSwitch = (direction) => {
     switch(direction) {
         case 'leftClockwise':
@@ -37,15 +34,14 @@ const Cube = () => {
             setBottom(([front[0], bottom[1], bottom[2], front[3],bottom[4], bottom[5], front[6], bottom[7], bottom[8] ]))
             setBack(([back[0], back[1], bottom[6], back[3], back[4], bottom[3], back[6], back[7], bottom[0] ]))
             setLeft(([left[2], left[5], left[8], left[1],left[4], left[7], left[0], left[3], left[6] ]))
-            console.log(front, back, right, top, left, bottom)
              break;  
-             case 'leftInverted':
+        case 'leftInverted':
              setFront(([bottom[0], front[1], front[2], bottom[3],front[4], front[5], bottom[6], front[7], front[8] ]))
              setTop(([front[0], top[1], top[2], front[3],top[4], top[5], front[6], top[7], top[8] ]))
              setBottom(([back[8], bottom[1], bottom[2], back[5],bottom[4], bottom[5], back[2], bottom[7], bottom[8] ]))
              setBack(([back[0], back[1], top[6], back[3], back[4], top[3], back[6], back[7], top[0] ]))
              setLeft(([left[6], left[3], left[0], left[7],left[4], left[1], left[8], left[5], left[2] ]))
-             console.log(front, back, right, top, left, bottom)
+         
              break;
         case 'rightInverted':
             setFront(([front[0], front[1], top[2], front[3],front[4], top[5], front[6], front[7], top[8] ]))
@@ -53,90 +49,79 @@ const Cube = () => {
             setBottom(([bottom[0], bottom[1], front[2], bottom[3],bottom[4], front[5], bottom[6], bottom[7], front[8] ]))
             setBack(([bottom[8], back[1], back[2], bottom[5], back[4], back[5], bottom[2], back[7], back[8] ]))
             setRight(([right[2], right[5], right[8], right[1],right[4], right[7], right[0], right[3], right[6] ]))
-            console.log(top, left, right, bottom, back, front)
              break;
-             case 'rightClockwise':
-                setFront(([front[0], front[1], bottom[2], front[3],front[4], bottom[5], front[6], front[7], bottom[8] ]))
-                setTop(([top[0], top[1], front[2], top[3],top[4], front[5], top[6], top[7], front[8] ]))
-                setBottom(([bottom[0], bottom[1], back[6], bottom[3],bottom[4], back[3], bottom[6], bottom[7], back[0] ]))
-                setBack(([top[8], back[1], back[2], top[5], back[4], back[5], top[2], back[7], back[8] ]))
-                setRight(([right[6], right[3], right[0], right[7],right[4], right[1], right[8], right[5], right[2] ]))
-                console.log(front, back, right, top, left, bottom)
-                break;
+        case 'rightClockwise':
+            setFront(([front[0], front[1], bottom[2], front[3],front[4], bottom[5], front[6], front[7], bottom[8] ]))
+            setTop(([top[0], top[1], front[2], top[3],top[4], front[5], top[6], top[7], front[8] ]))
+            setBottom(([bottom[0], bottom[1], back[6], bottom[3],bottom[4], back[3], bottom[6], bottom[7], back[0] ]))
+            setBack(([top[8], back[1], back[2], top[5], back[4], back[5], top[2], back[7], back[8] ]))
+            setRight(([right[6], right[3], right[0], right[7],right[4], right[1], right[8], right[5], right[2] ]))
+            break;
      
         case 'upInverted':
-              setFront(([left[2], left[1], left[0], front[3],front[4], front[5], front[6], front[7], front[8] ]))
+            setFront(([left[2], left[1], left[0], front[3],front[4], front[5], front[6], front[7], front[8] ]))
             setLeft(([back[2], back[1], back[0], left[3], left[4], left[5], left[6], left[7], left[8] ]))
             setRight(([front[0], front[1], front[2], right[3],right[4], right[5], right[6], right[7], right[8] ]))
             setBack(([right[0], right[1], right[2], back[3], back[4], back[5], back[6], back[7], back[8] ]))
             setTop(([top[2], top[5], top[8], top[1],top[4], top[7], top[0], top[3], top[6] ]))
-            console.log(front, back, right, top, left, bottom)
             break;
-            case 'upClockwise':
-                setFront(([right[0], right[1], right[2], front[3],front[4], front[5], front[6], front[7], front[8] ]))
-              setLeft(([front[2], front[1], front[0], left[3], left[4], left[5], left[6], left[7], left[8] ]))
-              setRight(([back[0], back[1], back[2], right[3],right[4], right[5], right[6], right[7], right[8] ]))
-              setBack(([left[2], left[1], left[0], back[3], back[4], back[5], back[6], back[7], back[8] ]))
-              setTop(([top[6], top[3], top[0], top[7],top[4], top[1], top[8], top[5], top[2] ]))
-              console.log(top, left, right, bottom, back, front)
-              break;
+        case 'upClockwise':
+            setFront(([right[0], right[1], right[2], front[3],front[4], front[5], front[6], front[7], front[8] ]))
+            setLeft(([front[2], front[1], front[0], left[3], left[4], left[5], left[6], left[7], left[8] ]))
+            setRight(([back[0], back[1], back[2], right[3],right[4], right[5], right[6], right[7], right[8] ]))
+            setBack(([left[2], left[1], left[0], back[3], back[4], back[5], back[6], back[7], back[8] ]))
+            setTop(([top[6], top[3], top[0], top[7],top[4], top[1], top[8], top[5], top[2] ]))
+            break;
          case 'downClockwise':
-                setFront(([front[0], front[1], front[2], front[3],front[4], front[5], left[8], left[7], left[6] ]))
-              setLeft(([left[0], left[1], left[2], left[3], left[4], left[5], back[8], back[7], back[6] ]))
-              setRight(([right[0], right[1], right[2], right[3],right[4], right[5], front[6], front[7], front[8] ]))
-              setBack(([back[0], back[1], back[2], back[3], back[4], back[5], right[6], right[7], right[8] ]))
-              setBottom([bottom[2], bottom[5], bottom[8], bottom[1], bottom[4], bottom[7], bottom[0], bottom[3], bottom[6]])
-              console.log(front, back, right, top, left, bottom)
-              break;
+            setFront(([front[0], front[1], front[2], front[3],front[4], front[5], left[8], left[7], left[6] ]))
+            setLeft(([left[0], left[1], left[2], left[3], left[4], left[5], back[8], back[7], back[6] ]))
+            setRight(([right[0], right[1], right[2], right[3],right[4], right[5], front[6], front[7], front[8] ]))
+            setBack(([back[0], back[1], back[2], back[3], back[4], back[5], right[6], right[7], right[8] ]))
+            setBottom([bottom[2], bottom[5], bottom[8], bottom[1], bottom[4], bottom[7], bottom[0], bottom[3], bottom[6]])
+            break;
         case 'downInverted':
-                setFront(([front[0], front[1], front[2], front[3],front[4], front[5], right[6], right[7], right[8] ]))
-                setLeft(([left[0], left[1], left[2], left[3], left[4], left[5], front[8], front[7], front[6] ]))
-                setRight(([right[0], right[1], right[2], right[3],right[4], right[5], back[6], back[7], back[8] ]))
-                setBack(([back[0], back[1], back[2], back[3], back[4], back[5], left[8], left[7], left[6] ]))
-                setBottom([bottom[6], bottom[3], bottom[0], bottom[7],bottom[4], bottom[1], bottom[8], bottom[5], bottom[2] ])
-                console.log(front, back, right, top, left, bottom)
-                break;
-        case 'frontFaceClockwise':
+            setFront(([front[0], front[1], front[2], front[3],front[4], front[5], right[6], right[7], right[8] ]))
+            setLeft(([left[0], left[1], left[2], left[3], left[4], left[5], front[8], front[7], front[6] ]))
+            setRight(([right[0], right[1], right[2], right[3],right[4], right[5], back[6], back[7], back[8] ]))
+            setBack(([back[0], back[1], back[2], back[3], back[4], back[5], left[8], left[7], left[6] ]))
+            setBottom([bottom[6], bottom[3], bottom[0], bottom[7],bottom[4], bottom[1], bottom[8], bottom[5], bottom[2] ])
+            break;
+        case 'frontClockwise':
             setTop(([top[0], top[1], top[2], top[3],top[4], top[5], left[6], left[3], left[0] ]))
             setLeft(([bottom[0], left[1], left[2], bottom[1], left[4], left[5], bottom[2], left[7], left[8] ]))
             setRight(([top[6], right[1], right[2], top[7],right[4], right[5], top[8], right[7], right[8] ]))
             setBottom(([right[6], right[3], right[0], bottom[3],bottom[4], bottom[5], bottom[6], bottom[7], bottom[8] ]))
             setFront(([front[6], front[3], front[0], front[7],front[4], front[1], front[8], front[5], front[2] ]))
-            console.log(top, left, right, bottom, back, front)
-         
             break;
-            case 'frontFaceAntiClockwise':
+        case 'frontInverted':
             setTop(([top[0], top[1], top[2], top[3],top[4], top[5], right[0], right[3], right[6] ]))
             setLeft(([top[8], left[1], left[2], top[7], left[4], left[5], top[6], left[7], left[8] ]))
             setRight(([bottom[2], right[1], right[2], bottom[1],right[4], right[5], bottom[2], right[7], right[8] ]))
             setBottom(([left[0], left[3], left[6], bottom[3],bottom[4], bottom[5], bottom[6], bottom[7], bottom[8] ]))
-            console.log(top, left, right, bottom, back, front)
+            setFront([front[2], front[5], front[8], front[1],front[4], front[7], front[0], front[3], front[6]])
+            
             break;
-            case 'bottomInverted':
+        case 'bottomInverted':
             setTop(([left[8], left[5], left[2],  top[3],top[4], top[5],  top[6], top[7], top[8] ]))
             setLeft([left[0], left[1], bottom[6],  left[3], left[4], bottom[7], left[6], left[7], bottom[8]])
             setRight([right[0], right[1], top[0], right[3], right[4], top[1], right[6], right[7], top[2]])
             setBottom([bottom[0], bottom[1], bottom[2], bottom[3], bottom[4], bottom[5], right[8], right[5], right[2]])
             setBack([back[2], back[5], back[8], back[1], back[4], back[7], back[0], back[3], back[6]])
-            console.log(front, back, right, top, left, bottom)
+          
             break;
-            case 'bottomClockwise':
-                setTop(([right[2], right[5], right[8],  top[3],top[4], top[5],  top[6], top[7], top[8] ]))
-                setLeft([left[0], left[1], top[2],  left[3], left[4], top[1], left[6], left[7], top[0]])
-                setRight([right[0], right[1], bottom[8], right[3], right[4], bottom[7], right[6], right[7], bottom[6]])
-                setBottom([bottom[0], bottom[1], bottom[2], bottom[3], bottom[4], bottom[5], left[2], left[5], left[8]])
-                setBack([back[6], back[3], back[0], back[7], back[4], back[1], back[8], back[5], back[2]])
-                console.log(front, back, right, top, left, bottom)
-                break;
+        case 'bottomClockwise':
+            setTop(([right[2], right[5], right[8],  top[3],top[4], top[5],  top[6], top[7], top[8] ]))
+            setLeft([left[0], left[1], top[2],  left[3], left[4], top[1], left[6], left[7], top[0]])
+            setRight([right[0], right[1], bottom[8], right[3], right[4], bottom[7], right[6], right[7], bottom[6]])
+            setBottom([bottom[0], bottom[1], bottom[2], bottom[3], bottom[4], bottom[5], left[2], left[5], left[8]])
+            setBack([back[6], back[3], back[0], back[7], back[4], back[1], back[8], back[5], back[2]])
+            break;
         default:
         
-          // code block
+        
       }
     
 }
-
-
-
     
     return (
         <>
